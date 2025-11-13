426aa437c40f0739f975609b5ead2a5ed12b2131
abbiebarnhardt19 <abbiebarnhardt@gmail.com>
2025-11-13 13:28:58 -0600

    more tutorial

--- Trendy Headaches/Styles/Reusable Components/Tutorial Components.swift
+++ Trendy Headaches/Styles/Reusable Components/Tutorial Components.swift
@@ -7,42 +7,6 @@
 
 import SwiftUI
 
-struct TutorialPopup: View {
-    var title: String
-    var message: String
-    var buttonText: String
-    var accent: String
-    var onNext: () -> Void
-
-    var body: some View {
-        ZStack {
-
-            
-            VStack(spacing: 16) {
-                Text(title)
-                    .font(.title3)
-                    .bold()
-                    .multilineTextAlignment(.center)
-
-                Text(message)
-                    .font(.body)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-
-                Button(buttonText) {
-                    print("button works")
-                }
-                .zIndex(1000)
-            }
-            .padding()
-            .frame(maxWidth: 300)
-            .background(Color(hex: accent))
-            .cornerRadius(16)
-            .shadow(radius: 10)
-        }
-    }
-}
-
 struct AnalyticsTutorialPopup: View {
     var bg: String
     var accent: String
@@ -57,22 +21,34 @@ struct AnalyticsTutorialPopup: View {
                 CustomText(text: "Analytics Page", color: bg, textAlign: .center, bold: true, textSize: 24)
                     .padding(.bottom, 5)
 
-                CustomText(text:"Get detailed insights into your headache patterns and triggers.", color: bg, width: screenWidth * 0.75, textAlign: .center, multiAlign: .center, textSize: 18)
-                    .padding(.bottom, 5)
-                
-                CustomText(text:"The Analytics page is split into three sections: Graphs, Stats, and Compare", color: bg, width: screenWidth * 0.7, textAlign: .center, multiAlign: .center, textSize: 18)
-                
-//                CustomText(text:"Graphs- see log statistics in visual format", color: bg, width: screenWidth * 0.7, textAlign: .center, multiAlign: .center, textSize: 18)
-                
-                CustomText(text:"Stats- see log statistics in numerical format", color: bg, width: screenWidth * 0.7, textAlign: .center, multiAlign: .center, textSize: 18)
-                
-                CustomText(text:"Compare- compare log statistics between symptoms, date ranges, and medications", color: bg, width: screenWidth * 0.7, textAlign: .center, multiAlign: .center, textSize: 18)
-                    .padding(.bottom, 5)
+                CustomText(text: "Get insights into your headache patterns and triggers. Filter logs by date, symptom, and type.",  color: bg, width: screenWidth * 0.8, textAlign: .leading, multiAlign: .leading, textSize: 18)
+                .padding(.bottom, 5)
+
+                CustomText(text: "The Analytics page has three sections: Graphs, Stats, and Compare.", color: bg, width: screenWidth * 0.80, textAlign: .center, multiAlign: .center, textSize: 18)
+
+                (Text("Graphs:").bold() + Text(" View your logs in charts and graphs."))
+                    .foregroundColor(Color(hex: bg))
+                    .font(.system(size: 18, design: .serif))
+                    .multilineTextAlignment(.center)
+                    .frame(width: screenWidth * 0.75)
+
+                (Text("Stats:").bold() + Text(" See your log data in numbers."))
+                    .foregroundColor(Color(hex: bg))
+                    .font(.system(size: 18, design: .serif))
+                    .multilineTextAlignment(.center)
+                    .frame(width: screenWidth * 0.75)
+
+                (Text("Compare:").bold() + Text(" Compare logs by symptom, date, or medication."))
+                    .foregroundColor(Color(hex: bg))
+                    .font(.system(size: 18, design: .serif))
+                    .multilineTextAlignment(.center)
+                    .frame(width: screenWidth * 0.75)
+
                 
                 CustomButton(text: "Next", bg: accent, accent: bg, height: 40, width: 90, textSize: 18, action: {print("Next")})
             }
             .padding()
-            .frame(width: screenWidth * 0.8)
+            .frame(width: screenWidth * 0.85)
             .background(Color(hex: accent))
             .cornerRadius(30)
             .shadow(radius: 10)
